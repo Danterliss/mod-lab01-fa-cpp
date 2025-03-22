1ee47b6e8d56d08a074be3c9ca9e19f06fd3022b
Семён Иванов <y61y51@gmail.com>
2025-03-22 12:24:26 +0300

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+    cout << faStr1("hjghg7jd 254886  fkgg:fd") << endl;
+    cout << faStr2("sdfghrey FGGRDS  Dfgrgr") << endl;
+    cout << faStr3("ewsrdfghj 12541 kjhjgf") << endl;
     return 0;
 }
