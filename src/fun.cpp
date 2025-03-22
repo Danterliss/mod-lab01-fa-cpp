11b3bae27de48b91e900f81d6e37d92f7c236801
Семён Иванов <y61y51@gmail.com>
2025-03-22 12:12:50 +0300

    update faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,26 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <cmath>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    bool inWord = false;
+    bool hasD = false;
+    while (*str) {
+        if (*str != ' ' && inWord == false) {
+            hasD = false;
+            if (isdigit(*str)) hasD = true;
+            inWord = true;
+        } else if (inWord == true && isdigit(*str)) {
+            hasD = true;
+        } else if (*str == ' ' && inWord == true) {
+            inWord = false;
+            if (!hasD) count++;
+        }
+        str++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
